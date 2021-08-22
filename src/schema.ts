e90511025d423daece0ec15f448cd3a840c5949d
George <giorgi.batsiashvili@geolab.edu.ge>
2021-08-22 23:20:52 +0400

    fix: open handlers for jest

---
+++ src/schema.ts
@@ -0,0 +1,23 @@
+const sqlite3 = require('sqlite3').verbose()
+export const db = new sqlite3.Database(':memory:')
+
+export const buildSchemas = (dbInsance: any) => {
+  const createRideTableSchema = `
+    CREATE TABLE Rides
+    (
+    rideID INTEGER PRIMARY KEY AUTOINCREMENT,
+    startLat DECIMAL NOT NULL,
+    startLong DECIMAL NOT NULL,
+    endLat DECIMAL NOT NULL,
+    endLong DECIMAL NOT NULL,
+    riderName TEXT NOT NULL,
+    driverName TEXT NOT NULL,
+    driverVehicle TEXT NOT NULL,
+    created DATETIME default CURRENT_TIMESTAMP
+    )
+  `
+  dbInsance.run(createRideTableSchema)
+  return db
+}
+
+
